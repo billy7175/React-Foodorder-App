8cf5fb34a3b5e2f9817e6a3280cba90bf4492562
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-06-19 00:13:07 +0900

    set routes

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-const morgan = require('morgan')
-require('dotenv').config();
+import express from "express";
+import cors from "cors";
+import { readdirSync } from "fs";
+const morgan = require("morgan");
+require("dotenv").config();
 
 // create express app
 const app = express();
-// apply middlewares
 
+// apply middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'))
-app.use((req,res,next) =>{
-  console.log('this is my own middleware')
-  next()
-})
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('you hit server endpoint');
-})
+// route
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-// const port = 8000
+// port
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 8000
-
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
