140321e6a3a29e43960285be1520b3825a7dfbdd
billy7175 <whgus439@gmail.com>
2022-06-21 00:11:14 +0900

    axios post request

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import axios from 'axios'
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.table({ name, email, password });
+    const { data } = await axios.post('http://localhost:8000/api/register', {
+      name, email, password
+    });
+    // http://localhost:8000/api/register
+    // http://localhost:8000/api/register
+    console.log('#data', data)
   };
 
   return (
