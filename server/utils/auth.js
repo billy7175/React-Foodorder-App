e8c77c0b2d9222ffaf71f4d9afeb04c575503a35
billy7175 <whgus439@gmail.com>
2022-06-22 10:04:34 +0900

    bcrypt untils

---
+++ server/utils/auth.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'
+
+// new Promise 사용
+// bcrypt.getSalt() 사용
+
+export const hashPassowrd = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(12, (error, salt) => {
+            if(error) { 
+                reject(error) 
+            }
+
+            bcrypt.hash(password, salt, (err, hashedPassword) => {
+                if(err) {
+                    reject(err)
+                }
+                resolve(hashedPassword)
+            })
+        })
+    })
+} 
+
+export const comparePassword = (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword)
+}
