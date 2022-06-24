521634895ffbebfa0aa4f34fe8d8e679643dbf43
billy7175 <whgus439@gmail.com>
2022-06-25 03:46:36 +0900

    login controller login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,11 +37,28 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
     try {
-        console.log(12312312321312)
-        console.log(req.body)
-        return res.json({ok: true})
-    } catch (error){
-        console.log(error)
-        return res.status(500).send("Error, Try Again");
+      // console.log(req.body);
+      const { email, password } = req.body;
+      // check if our db has user with that email
+      const user = await User.findOne({ email }).exec();
+      if (!user) return res.status(400).send("No user found");
+      // check password
+      const match = await comparePassword(password, user.password);
+      // create signed jwt
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      // return user and token to client, exclude hashed password
+      user.password = undefined;
+      // send token in cookie
+      res.cookie("token", token, {
+        httpOnly: true,
+        // secure: true, // only works on https
+      });
+      // send user as json response
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).send("Error. Try again.");
     }
-}
+  };
