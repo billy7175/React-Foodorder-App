03a1d9a014e1d0b43886fcd78b4107af64d72e76
billy7175 <whgus439@gmail.com>
2022-06-26 01:14:32 +0900

    verify token && get current user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,70 +5,80 @@ console.log('#hashPassowrd', hashPassowrd)
 console.log('#comparePassword', comparePassword)
 
 export const register = async (req, res) => {
-    try {
-        console.log('####req.body', req.body)
-        const {name, email, password} = req.body
-        if(!name) return res.status(400).send('Name is required');
-    
-        if(!password || password.length < 6) {
-            return res
-                    .status(400)
-                    .send('Password is required and should be min six characters')
-        }
-        let userExist = await User.findOne({email}).exec();
-        console.log('#userExist', userExist)
-        if(userExist) return res.status(400).send('Email is taken');
-    
-        const hashedPassword = await hashPassowrd(password)
-        
-        const user =  new User({
-            name,
-            email,
-            password : hashedPassword
-        })
-        await user.save()
-        console.log('@saved user', user)
-        return res.json({ok: true})
+  try {
+    console.log('####req.body', req.body)
+    const { name, email, password } = req.body
+    if (!name) return res.status(400).send('Name is required');
 
-     } catch (error) {
-        console.log(error);
-        return res.status(400).send("Error. Try again.");    
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send('Password is required and should be min six characters')
     }
+    let userExist = await User.findOne({ email }).exec();
+    console.log('#userExist', userExist)
+    if (userExist) return res.status(400).send('Email is taken');
+
+    const hashedPassword = await hashPassowrd(password)
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    })
+    await user.save()
+    console.log('@saved user', user)
+    return res.json({ ok: true })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Error. Try again.");
+  }
 }
 
 export const login = async (req, res) => {
-    try {
-      // console.log(req.body);
-      const { email, password } = req.body;
-      // check if our db has user with that email
-      const user = await User.findOne({ email }).exec();
-      if (!user) return res.status(400).send("No user found");
-      // check password
-      const match = await comparePassword(password, user.password);
-      // create signed jwt
-      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-      });
-      // return user and token to client, exclude hashed password
-      user.password = undefined;
-      // send token in cookie
-      res.cookie("token", token, {
-        httpOnly: true,
-        // secure: true, // only works on https
-      });
-      // send user as json response
-      res.json(user);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).send("Error. Try again.");
-    }
-  };
-
-  export const logout = async (req, res) => {
-      try {
-          res.clearCookie("token");
-          return res.json({message: "Sigout success"})
-      } catch (error) {
-        console.log('#logout', erorr)
-      }
+  try {
+    // console.log(req.body);
+    const { email, password } = req.body;
+    // check if our db has user with that email
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send("No user found");
+    // check password
+    const match = await comparePassword(password, user.password);
+    // create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // return user and token to client, exclude hashed password
+    user.password = undefined;
+    // send token in cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true, // only works on https
+    });
+    // send user as json response
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
   }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Sigout success" })
+  } catch (error) {
+    console.log('#logout', erorr)
+  }
+}
+
+export const currentUser = async (req,res) => {
+  try {
+    const user = await (await User.findById(req.user._id)).select('-password').exec();
+    console.log('CURRENT_USER', user)
+    return res.json(user);
+  } catch (error) {
+    console.log('#currentUser: error ', error)
+  }
+}
