b67e471b6dcf91a25280ae42908123b2428e22e4
billy7175 <whgus439@gmail.com>
2022-06-25 18:00:47 +0900

    conditinal nav links

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -3,34 +3,37 @@ import { Menu } from "antd";
 import Link from "next/link";
 import {
   AppstoreOutlined,
+  CoffeeOutlined,
   LoginOutlined,
-  ReadOutlined,
+  LogoutOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
-import { Context } from '../context'
-import axios from 'axios'
-import {useRouter} from 'next/router'
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
-const { Item } = Menu;
+const { Item, SubMenu } = Menu;
 
 const TopNav = () => {
   const [current, setCurrent] = useState("");
 
-  const {state, dispatch} = useContext(Context)
+  const { state, dispatch } = useContext(Context);
+  const { user } = state;
 
   const router = useRouter();
 
   useEffect(() => {
-    console.log('#process', process)
     process.browser && setCurrent(window.location.pathname);
   }, [process.browser && window.location.pathname]);
 
   const logout = async () => {
-    dispatch({ type: "LOGOUT"});
-    window.localStorage.removeItem("user")
-    const {data} = await axios.get('/api/logout')
-    router.push('/login')
-  }
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast(data.message);
+    router.push("/login");
+  };
 
   return (
     <Menu mode="horizontal" selectedKeys={[current]}>
@@ -44,33 +47,41 @@ const TopNav = () => {
         </Link>
       </Item>
 
-      <Item
-        key="/login"
-        onClick={(e) => setCurrent(e.key)}
-        icon={<LoginOutlined />}
-      >
-        <Link href="/login">
-          <a>Login</a>
-        </Link>
-      </Item>
+      {user === null && (
+        <>
+          <Item
+            key="/login"
+            onClick={(e) => setCurrent(e.key)}
+            icon={<LoginOutlined />}
+          >
+            <Link href="/login">
+              <a>Login</a>
+            </Link>
+          </Item>
 
-      <Item
-        key="/register"
-        onClick={(e) => setCurrent(e.key)}
-        icon={<UserAddOutlined />}
-      >
-        <Link href="/register">
-          <a>Register</a>
-        </Link>
-      </Item>
+          <Item
+            key="/register"
+            onClick={(e) => setCurrent(e.key)}
+            icon={<UserAddOutlined />}
+          >
+            <Link href="/register">
+              <a>Register</a>
+            </Link>
+          </Item>
+        </>
+      )}
 
-      <Item
-        onClick={logout}
-        icon={<UserAddOutlined />}
-        className="float-right"
-      >
-        Logout
-      </Item>
+      {user !== null && (
+        <SubMenu
+          icon={<CoffeeOutlined />}
+          title={user && user.name}
+          className="float-right"
+        >
+          <Item onClick={logout} className="float-right">
+            Logout
+          </Item>
+        </SubMenu>
+      )}
     </Menu>
   );
 };
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,7 @@
 import TopNav from "../components/TopNav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
-// import "../public/css/styles.css";
+import "../public/css/styles.css";
 import { Provider } from "../context";
 
 function MyApp({ Component, pageProps }) {
