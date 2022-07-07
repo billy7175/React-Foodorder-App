65392b49310d83d9b4a481079641e9b3d434e9f0
billy7175 <whgus439@gmail.com>
2022-07-08 02:11:07 +0900

    instructor protected page

--- server/controllers/instructor.js
+++ server/controllers/instructor.js
@@ -59,7 +59,7 @@ export const getAccountStatus = async (req, res) => {
 
 export const currentInstructor = async (req, res) => {
   try {
-    let user = await User.findById(req.user._id).select("-password").exec();
+    let user = await User.findById(req.auth._id).select("-password").exec();
     if (!user.role.includes("Instructor")) {
       return res.status(403);
     } else {
