f211edc4f0cfe3e0dd9a66e6f1353ff290902233
billy7175 <whgus439@gmail.com>
2022-07-10 22:18:03 +0900

    single course view for instructors

--- server/routes/course.js
+++ server/routes/course.js
@@ -6,11 +6,12 @@ const router = express.Router();
 import { requireSignin, isInstructor } from "../middlewares";
 
 // controllers
-import { uploadImage, removeImage, create } from "../controllers/course";
+import { uploadImage, removeImage, create,read } from "../controllers/course";
 
 router.post("/course/upload-image", uploadImage);
 router.post("/course/remove-image", removeImage);
 // course
 router.post("/course", requireSignin, isInstructor, create);
+router.get("/course/:slug", read);
 
 module.exports = router;
