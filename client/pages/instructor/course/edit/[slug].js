a9dbdf9347bb30302cf937d1e9d4b412d04863f1
billy7175 <whgus439@gmail.com>
2022-07-15 02:26:05 +0900

    update lesson component

--- client/pages/instructor/course/edit/[slug].js
+++ client/pages/instructor/course/edit/[slug].js
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 import { List, Avatar, Modal } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
+import UpdateLessonForm from "../../../../components/forms/UpdateLessonForm";
 
 const { Item } = List;
 
@@ -29,6 +30,11 @@ const CourseEdit = () => {
   // state for lessons update
   const [visible, setVisible] = useState(false);
   const [current, setCurrent] = useState({});
+  const [uploadVideoButtonText, setUploadVideoButtonText] = useState(
+    "Upload Video"
+  );
+  const [progress, setProgress] = useState(0);
+  const [uploading, setUploading] = useState(false);
 
   // router
   const router = useRouter();
@@ -141,6 +147,18 @@ const CourseEdit = () => {
     console.log("LESSON DELETED =>", data);
   };
 
+  /**
+   * lesson update functions
+   */
+
+  const handleVideo = () => {
+    console.log("handle video");
+  };
+
+  const handleUpdateLesson = () => {
+    console.log("handle update lesson");
+  };
+
   return (
     <InstructorRoute>
       <h1 className="jumbotron text-center square">Update Course</h1>
@@ -203,8 +221,16 @@ const CourseEdit = () => {
         onCancel={() => setVisible(false)}
         footer={null}
       >
-        update lesson form
-        <pre>{JSON.stringify(current, null, 4)}</pre>
+        <UpdateLessonForm
+          current={current}
+          setCurrent={setCurrent}
+          handleVideo={handleVideo}
+          handleUpdateLesson={handleUpdateLesson}
+          uploadVideoButtonText={uploadVideoButtonText}
+          progress={progress}
+          uploading={uploading}
+        />
+        {/* <pre>{JSON.stringify(current, null, 4)}</pre> */}
       </Modal>
     </InstructorRoute>
   );
