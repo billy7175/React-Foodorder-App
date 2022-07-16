46d1c639c84ebe35cebee525be4f6c032dafa410
billy7175 <whgus439@gmail.com>
2022-07-16 19:24:53 +0900

    video or image preview for single course

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from 'axios'
 import { useRouter } from 'next/router'
-import {Badge} from 'antd'
+import {Badge, Modal} from 'antd'
 import { currencyFormatter } from "../../utils/helpers";
+import ReactPlayer from 'react-player'
 
 const SingleCourse = ({ course }) => {
+    // state
+    const [showModal, setShowModal] = useState(false)
+    const [preview, setPreview]= useState("");
+
     const router = useRouter();
     const { slug } = router.query
     const { name, description, instructor, updateAt, lessons, image, price, paid, category } = course
@@ -35,6 +40,23 @@ const SingleCourse = ({ course }) => {
                         }</h4>
                     </div>
                     <div className="col-md-4">
+                        {JSON.stringify(lessons[0])}
+                        {lessons[0].video && lessons[0].video.Location ? 
+                        (<div onClick={() => {
+                            setPreview(lessons[0].video.Location)
+                            setShowModal(!showModal)
+                        }}>
+                            <ReactPlayer className="react-player-div" 
+                                url={lessons[0].video.Location}
+                                light={image.Location}
+                                width="100%"
+                                height="225px"
+                            />
+                        </div>) : (<div>
+                        
+                                <img src = {image.Location} alt={name} className="img img-fluid" />
+                            
+                        </div>)}
                         <p>show course image</p>
                         <p>show enroll button</p>
                         {/* show video preview or coure image */}
@@ -42,6 +64,8 @@ const SingleCourse = ({ course }) => {
                     </div>
                 </div>
             </div>
+
+            {showModal ? course.lessons[0].video.Location : "don't show"}
 
         </>
     )
