adfb045e07980f46d2e4f4ac59f4c7d8d92c1a18
billy7175 <whgus439@gmail.com>
2022-07-16 19:05:24 +0900

    display single course info

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -1,16 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from 'axios'
 import { useRouter } from 'next/router'
+import {Badge} from 'antd'
+import { currencyFormatter } from "../../utils/helpers";
 
-const SingleCourse = ({course}) => {
+const SingleCourse = ({ course }) => {
     const router = useRouter();
     const { slug } = router.query
+    const { name, description, instructor, updateAt, lessons, image, price, paid, category } = course
 
     return (
         <>
-            <div className="container-fluid">
-                <div className="row">{JSON.stringify(course,null,4)}</div>
+
+            <div class="jumbotron bg-primary square">
+                <div className="row">
+                    <div className="col-md-8">
+                        <h1 className="text-light font-weight-bold">{name}</h1>
+                        <div className="lead">
+                            {description && description.substring(0, 160)}...
+                        </div>
+                        {/* title */}
+
+                        <Badge count={category} style={{background: "#03a964"}} className="pd-4 mr-2" />
+                        {/* title */}
+                        <p>Created by {instructor.name}</p>
+                        {/* title */}
+                        <p>Last updated {new Date(updateAt).toLocaleDateString()}</p>
+                        {/* title */}
+                        <h4 className="text-light">{
+                            paid ? currencyFormatter({
+                                amount: price,
+                                currency : 'use'
+                            }) : 'Free'
+                        }</h4>
+                    </div>
+                    <div className="col-md-4">
+                        <p>show course image</p>
+                        <p>show enroll button</p>
+                        {/* show video preview or coure image */}
+                        {/* enroll button */}
+                    </div>
+                </div>
             </div>
+
         </>
     )
 };
