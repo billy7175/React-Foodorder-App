c57de58cafe21462b3612426a5a41933ca463a96
billy7175 <whgus439@gmail.com>
2022-07-16 17:09:11 +0900

    publish courses in main page for instructor

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
 const Index = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const {data} = await axios.get('/api/courses');
+            setCourses(data);
+        }
+
+        fetchCourses();
+    }, [])
+
     return (
         <>
             <h1 className="jumbotron text-center bg-primary">Online Education Market</h1>
-            <p>from next js...</p>
+            <div className="container-fluid">
+                <div className="row">
+                    { courses.map(c => (
+                        <div className="col-md-4" key={c._id}>
+                            <pre>{JSON.stringify(c, null ,4)}</pre>
+                        </div>
+                    ))}
+                </div> 
+            </div>
         </>
     )
 }
